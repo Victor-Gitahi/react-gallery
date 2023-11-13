e9c9e5cc1512709a4bc378e5d6d3e2e4b84a1e20
gitahi-pixels <gitahi14043@student.mmarau.ac.ke>
2023-11-13 03:37:14 +0300

    Gallery implemented using JSX

---
+++ src/data/FilterableData.ts
@@ -0,0 +1,62 @@
+export const filterableData = [
+    {
+        name: "Balerina",
+        src: "./src/images/ballerina.png",
+        title: "Ballerina",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Balerina ",
+        src: "./src/images/ballerina 2.png",
+        title: "Ballerina",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Abstract Art",
+        src: "./src/images/abstract art.png",
+        title: "Abstract Art",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Abstract Art",
+        src: "./src/images/abstract art 2.png",
+        title: "Abstract Art",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Abstract Art",
+        src: "./src/images/abstract art 3.png",
+        title: "Abstract Art",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Abstract Art",
+        src: "./src/images/abstract art 2.jpg",
+        title: "Abstract Art",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Cyberpunk",
+        src: "./src/images/cyberpunk.jpg",
+        title: "Cyberpunk",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Cyberpunk",
+        src: "./src/images/cyberpunk 2.png",
+        title: "Cyberpunk",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Portrait",
+        src: "./src/images/portrait colors splash.jpg",
+        title: "Portrait",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        name: "Portrait",
+        src: "./src/images/portrait colors.jpg",
+        title: "Portrait",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+]
